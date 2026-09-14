f3010cb2c00327143aea968707a6f8e1895841dc
JonoChandler <jonathanpchandler110@gmail.com>
2026-09-14 22:04:53 +1000

    Add new maths-ext1 worksheet resource

--- data.js
+++ data.js
@@ -139,3 +139,11 @@ SUBJECTS.forEach((subject) => {
     });
   });
 });
+RESOURCES.push({
+  subject: "maths-ext1",
+  year: 12,
+  topic: "Proof",
+  title: "Mathematical Induction - Challenging Questions",
+  type: "Worksheet",
+  file: "resources/maths-ext1/Mathematical Induction - Challenging Questions.pdf",
+});
